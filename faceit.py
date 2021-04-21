2ae4d25e3c4f3c03c26b9a84cf5b3e1fe5bbc1a1
Simon Brage <simonbrage@live.dk>
2021-04-21 17:13:42 +0200

    Revert # dotenv

--- faceit.py
+++ faceit.py
@@ -1,9 +1,9 @@
 import os
 import requests
 import json
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-#load_dotenv()
+load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
 header = {'accept': 'application/json', 'Authorization': 'Bearer {}'.format(API_KEY)}
