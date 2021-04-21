583f781fbdb315e50edd5436dffb587446d09e5d
Simon Brage <simonbrage@live.dk>
2021-04-21 17:01:12 +0200

    Initial commit

---
+++ faceit.py
@@ -0,0 +1,130 @@
+import os
+import requests
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+header = {'accept': 'application/json', 'Authorization': 'Bearer {}'.format(API_KEY)}
+url = 'https://open.faceit.com/data/v4'
+
+# PLAYER INFORMATION
+def faceit_get_player_id(nickname):
+    query_params = {'nickname': nickname}
+    response = requests.get(url + '/players', headers=header, params=query_params)
+
+    return response.json()['player_id']
+
+def faceit_get_player_country(nickname):
+    query_params = {'nickname': nickname}
+    response = requests.get(url + '/players', headers=header, params=query_params)
+
+    return response.json()['country']
+
+def faceit_get_player_avatar(nickname):
+    query_params = {'nickname': nickname}
+    response = requests.get(url + '/players', headers=header, params=query_params)
+
+    return response.json()['avatar']
+
+def faceit_get_player_level(nickname):
+    query_params = {'nickname': nickname}
+    response = requests.get(url + '/players', headers=header, params=query_params)
+
+    return response.json()['games']['csgo']['skill_level']
+
+def faceit_get_player_elo(nickname):
+    query_params = {'nickname': nickname}
+    response = requests.get(url + '/players', headers=header, params=query_params)
+
+    return response.json()['games']['csgo']['faceit_elo']
+
+def faceit_get_player_infraction_date(nickname):
+    query_params = {'nickname': nickname}
+    response = requests.get(url + '/players', headers=header, params=query_params)
+
+    return response.json()['infractions']['last_infraction_date']
+
+# PLAYER LIFETIME STATS
+def faceit_get_lifetime_stats(player_id):
+    query_params = {'player_id': player_id, 'game_id': 'csgo'}
+    response = requests.get(url + '/players/{}/stats/{}'.format(player_id, 'csgo'), headers=header, params=query_params)
+
+    return response.json()['lifetime']
+
+def faceit_get_lifetime_stats_total_matches(player_id):
+    query_params = {'player_id': player_id, 'game_id': 'csgo'}
+    response = requests.get(url + '/players/{}/stats/{}'.format(player_id, 'csgo'), headers=header, params=query_params)
+
+    return response.json()['lifetime']['Matches']
+
+def faceit_get_lifetime_stats_kd_ratio(player_id):
+    query_params = {'player_id': player_id, 'game_id': 'csgo'}
+    response = requests.get(url + '/players/{}/stats/{}'.format(player_id, 'csgo'), headers=header, params=query_params)
+
+    return response.json()['lifetime']['Average K/D Ratio']
+
+def faceit_get_lifetime_stats_win_rate(player_id):
+    query_params = {'player_id': player_id, 'game_id': 'csgo'}
+    response = requests.get(url + '/players/{}/stats/{}'.format(player_id, 'csgo'), headers=header, params=query_params)
+
+    return response.json()['lifetime']['Win Rate %']
+
+def faceit_get_lifetime_stats_hs_rate(player_id):
+    query_params = {'player_id': player_id, 'game_id': 'csgo'}
+    response = requests.get(url + '/players/{}/stats/{}'.format(player_id, 'csgo'), headers=header, params=query_params)
+
+    return response.json()['lifetime']['Average Headshots %']
+
+def faceit_get_lifetime_stats_recent_results(player_id):
+    query_params = {'player_id': player_id, 'game_id': 'csgo'}
+    response = requests.get(url + '/players/{}/stats/{}'.format(player_id, 'csgo'), headers=header, params=query_params)
+
+    results = response.json()['lifetime']['Recent Results']
+    results_string = ''
+    for r in results:
+        if r == '1':
+            results_string += '**W** '
+        else:
+            results_string += 'L '
+    
+    return results_string
+
+def faceit_get_win_streak(player_id):
+    query_params = {'player_id': player_id, 'game_id': 'csgo'}
+    response = requests.get(url + '/players/{}/stats/{}'.format(player_id, 'csgo'), headers=header, params=query_params)
+
+    return response.json()['lifetime']['Current Win Streak']
+
+def faceit_get_longest_win_streak(player_id):
+    query_params = {'player_id': player_id, 'game_id': 'csgo'}
+    response = requests.get(url + '/players/{}/stats/{}'.format(player_id, 'csgo'), headers=header, params=query_params)
+
+    return response.json()['lifetime']['Longest Win Streak']
+
+# PLAYER RANKINGS    
+def faceit_get_country_ranking(player_id, country):
+    query_params = {'player_id': player_id, 'game_id': 'csgo', 'region': 'EU', 'country': country}
+    response = requests.get(url + '/rankings/games/{}/regions/{}/players/{}'.format('csgo', 'EU', player_id), headers=header, params=query_params)
+
+    return response.json()['position']
+
+def faceit_get_region_ranking(player_id):
+    query_params = {'player_id': player_id, 'game_id': 'csgo', 'region': 'EU', 'country': ''}
+    response = requests.get(url + '/rankings/games/{}/regions/{}/players/{}'.format('csgo', 'EU', player_id), headers=header, params=query_params)
+
+    return response.json()['position']
+
+# PLAYER MATCHES AND MATCH STATISTICS        
+def faceit_get_matches(player_id):
+    query_params = {'player_id': player_id, 'game_id': 'csgo', 'offset': '0', 'limit': '20'}
+    response = requests.get(url + '/players/{}/history'.format(player_id), headers=header, params=query_params)
+
+    return response.json()
+
+def faceit_get_match_stats(match_id):
+    query_params = {'match_id': match_id}
+    response = requests.get(url + '/matches/{}/stats'.format(match_id), headers=header, params=query_params)
+
+    return response.json()
---
+++ main.py
@@ -0,0 +1,149 @@
+import os
+import discord
+import datetime
+from dotenv import load_dotenv
+from discord.ext import commands
+from faceit import *
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='-')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is connected.')
+
+# ----------------------------------------------
+# ------------------ COMMANDS ------------------
+# ----------------------------------------------
+@bot.command(name='backflip', help='Displays a backflip GIF.')
+async def backflip(ctx):
+    await ctx.send('https://giphy.com/gifs/officialfiym-forever-in-your-mind-fiym-xT0xetpPHT8UryiiqY')
+
+# Displays lifetime stats, current ranking, and results of last five games (W or L)
+@bot.command(name='profile', help='Displays Faceit profile of a player.')
+async def profile(ctx, nickname):
+    if nickname.lower() == 'doomyo': nickname = 'Doomyo'
+    elif nickname.lower() == 'grunk' or nickname.lower() == 'grunk_' or nickname.lower() == 'grundt': nickname = 'GrunK_'
+    elif nickname.lower() == 'brage' or nickname.lower() == 'bragi' or nickname.lower() == 'goat': nickname = 'bragi'
+
+    date = datetime.datetime.now()
+
+    player_id = faceit_get_player_id(nickname)
+    player_country = faceit_get_player_country(nickname)
+    player_avatar = faceit_get_player_avatar(nickname)
+    
+    player_level = faceit_get_player_level(nickname)
+    player_elo = faceit_get_player_elo(nickname)
+
+    total_matches_played = faceit_get_lifetime_stats_total_matches(player_id)
+    kd_ratio = faceit_get_lifetime_stats_kd_ratio(player_id)
+    win_rate = faceit_get_lifetime_stats_win_rate(player_id)
+    hs_rate = faceit_get_lifetime_stats_hs_rate(player_id)
+    results = faceit_get_lifetime_stats_recent_results(player_id)
+    longest_win_streak = faceit_get_longest_win_streak(player_id)
+
+    country_ranking = faceit_get_country_ranking(player_id, player_country)
+    region_ranking = faceit_get_region_ranking(player_id)
+
+    embed=discord.Embed(title='Career - **{}**'.format(nickname),  url='https://www.faceit.com/en/players/{}'.format(nickname), color=0x824dff)
+    embed.set_thumbnail(url=player_avatar)
+    embed.add_field(name='Level', value=player_level, inline=True)
+    embed.add_field(name='ELO', value=player_elo, inline=True)
+    embed.add_field(name='Matches', value=total_matches_played, inline=True)
+    embed.add_field(name='⠀', value='━━━━━━━━━━━━━━━', inline=False)
+    embed.add_field(name='K/D', value=kd_ratio, inline=True)
+    embed.add_field(name='HS%', value=hs_rate + '%', inline=True)
+    embed.add_field(name='Win%', value=win_rate + '%', inline=True)
+    embed.add_field(name='Best Win Streak', value=longest_win_streak + " games", inline=True)
+    embed.add_field(name='Past results', value=results, inline=True)
+    embed.add_field(name='⠀', value='━━━━━━━━━━━━━━━', inline=False)
+    embed.add_field(name='Ranking :flag_dk:', value=country_ranking, inline=True)
+    embed.add_field(name='Ranking :flag_eu:', value=region_ranking, inline=True)
+    embed.set_footer(text=date)
+
+    await ctx.send(embed=embed)
+
+# Displays player stats from last twenty matches.
+@bot.command(name='stats', help='Displays stats of last 20 matches.')
+async def stats(ctx, nickname):
+    if nickname.lower() == 'doomyo': nickname = 'Doomyo'
+    elif nickname.lower() == 'grunk' or nickname.lower() == 'grunk_' or nickname.lower() == 'grundt': nickname = 'GrunK_'
+    elif nickname.lower() == 'brage' or nickname.lower() == 'bragi' or nickname.lower() == 'goat': nickname = 'bragi'
+
+    date = datetime.datetime.now()
+
+    player_id = faceit_get_player_id(nickname)
+    player_country = faceit_get_player_country(nickname)
+    player_avatar = faceit_get_player_avatar(nickname)
+    player_level = faceit_get_player_level(nickname)
+    player_elo = faceit_get_player_elo(nickname)
+
+    results = faceit_get_lifetime_stats_recent_results(player_id)
+
+    country_ranking = faceit_get_country_ranking(player_id, player_country)
+    region_ranking = faceit_get_region_ranking(player_id)
+
+    match_list = faceit_get_matches(player_id)['items']
+    matches_length = len(match_list)
+
+    avg_kills = 0.0
+    avg_kr_ratio = 0.0
+    avg_hs_ratio = 0.0
+    avg_kd_ratio = 0.0
+    win_rate = 0.0
+    
+    for m in range(matches_length):
+        match_id = match_list[m]['match_id']
+        player = faceit_get_match_stats(match_id)['rounds'][0]['teams']
+        for p in range(2):
+            for i in range(5):
+                if player[p]['players'][i]['player_id'] == player_id:
+                    avg_kills += float(player[p]['players'][i]['player_stats']['Kills'])
+                    avg_kr_ratio += float(player[p]['players'][i]['player_stats']['K/R Ratio'])
+                    avg_hs_ratio += float(player[p]['players'][i]['player_stats']['Headshots %'])
+                    avg_kd_ratio += float(player[p]['players'][i]['player_stats']['K/D Ratio'])
+                    win_rate += float(player[p]['players'][i]['player_stats']['Result'])
+    avg_kills = round((avg_kills/matches_length))
+    avg_kr_ratio = round((avg_kr_ratio/matches_length),2)
+    avg_hs_ratio = str(round((avg_hs_ratio/matches_length)))
+    avg_kd_ratio = round((avg_kd_ratio/matches_length),2)
+    win_rate = str(round(win_rate*(100/matches_length)))
+
+    embed=discord.Embed(title='Last {} matches - **{}**'.format(matches_length, nickname),  url='https://www.faceit.com/en/players/{}'.format(nickname), color=0x824dff)
+    embed.set_thumbnail(url=player_avatar)
+    embed.add_field(name='Level', value=player_level, inline=True)
+    embed.add_field(name='ELO', value=player_elo, inline=True)
+    embed.add_field(name='⠀', value='━━━━━━━━━━━━━━━', inline=False)
+    embed.add_field(name='Avg. kills', value=avg_kills, inline=True)
+    embed.add_field(name='Avg. HS%', value=avg_hs_ratio + '%', inline=True)
+    embed.add_field(name='Avg. K/D', value=avg_kd_ratio, inline=True)
+    embed.add_field(name='Avg. K/R', value=avg_kr_ratio, inline=True)
+    embed.add_field(name='Win%', value=win_rate + '%', inline=True)
+    embed.add_field(name='Past results', value=results, inline=True)
+    embed.add_field(name='⠀', value='━━━━━━━━━━━━━━━', inline=False)
+    embed.add_field(name='Ranking :flag_dk:', value=country_ranking, inline=True)
+    embed.add_field(name='Ranking :flag_eu:', value=region_ranking, inline=True)
+    embed.set_footer(text=date)
+
+    await ctx.send(embed=embed)
+
+
+# Displays infractions (AFK, leave, no check-in)
+@bot.command(name='infractions', help='How naughty have you been?')
+async def infractions(ctx, nickname):
+    if nickname.lower() == 'doomyo': nickname = 'Doomyo'
+    elif nickname.lower() == 'grunk' or nickname.lower() == 'grunk_' or nickname.lower() == 'grundt': nickname = 'GrunK_'
+    elif nickname.lower() == 'brage' or nickname.lower() == 'bragi' or nickname.lower() == 'goat': nickname = 'bragi'
+
+    date = faceit_get_player_infraction_date(nickname)
+    if date == '':
+        await ctx.send('**{}** has **0** infractions. :partying_face:'.format(nickname))
+    else:
+        afk = date['afk']
+        leaver = date['leaver']
+        not_checked_in = date['qm_not_checkedin']
+        await ctx.send('**{}\'s** last infraction was on {}\n  AFK: {}\n  Leaver: {}\n  Not checked in: {}'.format(nickname, date, afk, leaver, not_checked_in))
+
+bot.run(TOKEN)
