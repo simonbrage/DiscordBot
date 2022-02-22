8be19f7bf11d9887fa3262ec0b4a9ceb52d0c92e
Simon Brage <simonbrage@live.dk>
2022-02-22 16:10:22 +0100

    Changed argument name for better readability in Discord

--- main.py
+++ main.py
@@ -39,13 +39,13 @@ class Miscellaneous(commands.Cog):
     @commands.command(name='roll', 
                 description= 'Pick a random number between 1 and the given number. Defaults to 1 and 6 if no number is given.', 
                 help='Pick a random number between 1 and your number.')
-    async def roll(self, ctx, num=None):
-        if num == None: 
+    async def roll(self, ctx, number=None):
+        if number == None: 
             await ctx.send('{} rolled **{}** (1-6)'.format(ctx.message.author.mention, dice_roll()))
             await ctx.message.delete(delay=2)
             return 
         try:
-            await ctx.send('{} rolled **{}** (1-{})'.format(ctx.message.author.mention, custom_dice_roll(num), num))
+            await ctx.send('{} rolled **{}** (1-{})'.format(ctx.message.author.mention, custom_dice_roll(number), number))
             await ctx.message.delete(delay=2)
         except ValueError:
             await ctx.message.delete(delay=5)
