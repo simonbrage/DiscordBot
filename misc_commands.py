fb418af4194a31f6ef5fda7fd28184c527981975
Simon Brage <simonbrage@live.dk>
2022-03-08 20:50:34 +0100

    Moved gifs dictionary to separate file and added more gifs
    Gifs dictionary

--- misc_commands.py
+++ misc_commands.py
@@ -3,11 +3,7 @@
 import random
 
 def backflip_gif():
-    gifs = ['https://giphy.com/gifs/justin-flipping-back-flip-jgflips-gczbF6nbRPVZ1YigE3',
-            'https://giphy.com/gifs/officialfiym-forever-in-your-mind-fiym-xT0xetpPHT8UryiiqY',
-            'https://giphy.com/gifs/fail-backflip-faceplant-sLFxTuBNHQfPW',
-            'https://giphy.com/gifs/perfect-loops-3Wv7L5PaTBtAfrLSul',
-            'https://giphy.com/gifs/espncfb-3oz8xUGwps7hlUMhaM']
+    from resources.gifs import gifs
 
     return random.choice(gifs)
 
---
+++ resources/gifs.py
@@ -0,0 +1,14 @@
+# Dictionary of GIFs
+
+gifs = ['https://giphy.com/gifs/justin-flipping-back-flip-jgflips-gczbF6nbRPVZ1YigE3',
+            'https://giphy.com/gifs/officialfiym-forever-in-your-mind-fiym-xT0xetpPHT8UryiiqY',
+            'https://giphy.com/gifs/fail-backflip-faceplant-sLFxTuBNHQfPW',
+            'https://giphy.com/gifs/perfect-loops-3Wv7L5PaTBtAfrLSul',
+            'https://giphy.com/gifs/espncfb-3oz8xUGwps7hlUMhaM',
+            'https://giphy.com/clips/Ligue1UberEats-UAAEdMMX8mvtxyfVbp',
+            'https://giphy.com/clips/paralympics-yes-flip-ja-k8Gaui8LSd3pTfwuh8',
+            'https://giphy.com/gifs/moodman-fail-life-backflip-YTJXDIivNMPuNSMgc0',
+            'https://giphy.com/gifs/afv-funny-fail-3o85xz8c70Ba4W8TCg',
+            'https://giphy.com/gifs/surfing-trick-like-a-boss-VZI4E0l3ZOEpO',
+            'https://giphy.com/gifs/foxsports-nascar-carl-edwards-bristol-motor-speedway-SdcF9mdkQ4Bzi',
+            'https://giphy.com/gifs/bellatormma-xT9DPB84gCztS8pBQs']
